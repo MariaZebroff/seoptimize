1ce086afe5677e32b4d1967dd95eea999d76715f
MariaZebroff <108751599+MariaZebroff@users.noreply.github.com>
2025-09-19 09:46:55 -0700

    Force rebuild with environment variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // Force rebuild with environment variables
   return (
     <html lang="en">
       <body
